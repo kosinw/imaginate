0682ef6cfdc0ccde8392d200f3e77697dd362ad4
byrdm2000 <byrdm@mit.edu>
2022-01-11 15:58:30 -0500

    Added database config

--- server/server.js
+++ server/server.js
@@ -33,9 +33,10 @@ const socketManager = require("./server-socket");
 
 // Server configuration below
 // TODO change connection URL after setting up your team database
-const mongoConnectionURL = "FILL ME IN";
+const mongoConnectionURL =
+  "mongodb+srv://admin:kjePoYiJrW1KmEaZ@cluster0.pnm5o.mongodb.net/imaginate?retryWrites=true&w=majority";
 // TODO change database name to the name you chose
-const databaseName = "FILL ME IN";
+const databaseName = "imaginate";
 
 // connect to mongodb
 mongoose
