a6af4dcf5843a3c215ea67979d904410a843288b
Kosi Nwabueze <kosinwabueze@gmail.com>
2022-01-11 18:21:05 -0500

    created animation router at /api/animations

---
+++ client/src/hooks/useUser.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { get, post } from '../utilities';
+
+const useUser = () => {
+  const [userId, setUserId] = React.useState(null);
+
+  React.useEffect(() => {
+    get("/api/whoami").then((user) => {
+      if (user._id) {
+        setUserId(user._id);
+      }
+    });
+  }, []);
+
+  const handleLogin = (res) => {
+    console.log(`Logged in as ${res.profileObj.name}`);
+    const userToken = res.tokenObj.id_token;
+    post("/api/login", { token: userToken }).then((user) => {
+      setUserId(user._id);
+      post("/api/initsocket", { socketid: socket.id });
+    });
+  };
+
+  const handleLogout = () => {
+    setUserId(undefined);
+    post("/api/logout");
+  };
+
+  return {
+    userId,
+    handleLogin,
+    handleLogout
+  };
+}
+
+export default useUser;
