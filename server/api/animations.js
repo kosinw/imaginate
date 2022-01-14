410ee8d1d96cb91f3137211bd944986fe65d4662
Matthew Byrd <byrdm2000@gmail.com>
2022-01-13 21:57:05 -0500

    Refactored animations api to use callbacks, added animationlist to front page

--- server/api/animations.js
+++ server/api/animations.js
@@ -5,14 +5,20 @@ const router = express.Router();
 
 // TODO(kosi): Add filtering options (query strings)
 router.get("/", async (req, res) => {
-  const animations = await Animation.find({}).execPopulate("creator");
-  return res.status(200).json(animations);
+  Animation.find({})
+    .populate("creator")
+    .then((animations) => {
+      return res.status(200).json(animations);
+    });
 });
 
 router.get("/:animationId", async (req, res) => {
   const { animationId } = req.params;
-  const animation = await Animation.findOne({ _id: animationId }).execPopulate("creator");
-  return res.status(200).json(animation);
+  Animation.findOne({ _id: animationId })
+    .populate("creator")
+    .then((animation) => {
+      return res.status(200).json(animation);
+    });
 });
 
 module.exports = router;
