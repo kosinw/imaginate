7cf6b373f34f347185c95dbeeb60f5ac778566af
Matthew Byrd <byrdm2000@gmail.com>
2022-01-17 22:15:20 -0500

    Added title to AnimationPreview

--- client/src/components/modules/AnimationPreview.js
+++ client/src/components/modules/AnimationPreview.js
@@ -15,21 +15,24 @@ const AnimationPreview = ({ animation, userId }) => {
         <img className="thumbnail" src={animation.thumbnail} />
       </Link>
       <div className="info-container">
-        <figcaption className="caption-container">
-          <Identicon className="caption-icon" size={24} value={animation.creator.name} />
-          <Link to={`/profile/${animation.creator._id}`}>
-            <span className="caption-title">{animation.creator.name}</span>
-          </Link>
-        </figcaption>
-        <div className="upvote-container">
-          <button
-            type="button"
-            aria-label="Upvote"
-            className={classnames({ "upvote-button": true, active: active })}
-          >
-            <HiArrowSmUp />
-            <span className="upvote-count">{animation.score}</span>
-          </button>
+        <span className="info-title">{animation.title || "Untitled"}</span>
+        <div className="action-container">
+          <figcaption className="caption-container">
+            <Identicon className="caption-icon" size={24} value={animation.creator.name} />
+            <Link to={`/profile/${animation.creator._id}`}>
+              <span className="caption-title">{animation.creator.name}</span>
+            </Link>
+          </figcaption>
+          <div className="upvote-container">
+            <button
+              type="button"
+              aria-label="Upvote"
+              className={classnames({ "upvote-button": true, active: active })}
+            >
+              <HiArrowSmUp />
+              <span className="upvote-count">{animation.score}</span>
+            </button>
+          </div>
         </div>
       </div>
     </figure>
