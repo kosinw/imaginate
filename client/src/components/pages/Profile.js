aef69e2dd28554e7d067d64072ce8256f77964b7
Matthew Byrd <byrdm2000@gmail.com>
2022-01-24 00:14:28 -0500

    Added sorting to user profile page

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import useSWR from "swr";
 
 import fetcher from "../../lib/utils/fetcher";
+import SortPicker from "../modules/SortPicker";
 import AnimationPreviewGridView from "../modules/AnimationPreviewGridView";
 
 /**
@@ -12,9 +13,16 @@ import AnimationPreviewGridView from "../modules/AnimationPreviewGridView";
  */
 const Profile = ({ userId }) => {
   const { data: user } = useSWR(userId && `/api/users/${userId}`, fetcher);
+  const [sort, setSort] = useState("score");
 
   // TODO(kosi): Input spinner + skeleton cards.
-  if (!user) { return <div>Loading...</div> }
+  if (!user) {
+    return <div>Loading...</div>;
+  }
+
+  const handleSortChange = (newSort) => {
+    setSort(newSort);
+  };
 
   return (
     <main className="Page Page--Index">
@@ -22,8 +30,11 @@ const Profile = ({ userId }) => {
         <h1 className="Page--Index__title">
           Currently browsing <span className="text-primary">{user.name}'s</span> profile page.
         </h1>
+        <div className="Page--Index__options">
+          <SortPicker handleSortChange={handleSortChange} />
+        </div>
       </div>
-      <AnimationPreviewGridView resource={`/api/users/${userId}/animations`} />
+      <AnimationPreviewGridView resource={`/api/users/${userId}/animations` + `?order=${sort}`} />
     </main>
   );
 };
