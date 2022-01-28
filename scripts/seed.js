78ddd56c9151b56ae6334bd328c10d0254e5b8f0
Kosi Nwabueze <kosinwabueze@gmail.com>
2022-01-27 21:48:13 -0500

    added seed script

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,87 +1,45 @@
-const User = require("../server/models/user");
-const Animation = require("../server/models/animation");
-const Frame = require("../server/models/frame");
 const connect = require("../server/database");
-const faker = require("@faker-js/faker");
-const sampleSize = require("lodash.samplesize");
+const { faker } = require("@faker-js/faker");
+const cuid = require('cuid');
 
-/**
- * Seeds users. Returns list of all users.
- * @param {Number} n number of users to seed
- */
-async function seedUsers(n = 10) {
-  await User.deleteMany();
+const AnimationsController = require("../server/controllers/animations");
+const User = require("../server/models/user");
 
-  let users = [];
+async function seedDatum() {
+  const user = new User({
+    name: faker.name.firstName(),
+    googleid: cuid()
+  });
 
-  for (let i = 0; i < n; ++i) {
-    const user = new User({
-      name: faker.name.findName(),
-      googleid: faker.mersenne.rand(11214843000, 113512389000),
+  await user.save();
+
+  const animation = await AnimationsController.create({
+    creator: user._id,
+    framerate: 3,
+    title: faker.company.bsNoun(),
+    resolution: {
+      width: 640,
+      height: 360
+    },
+  });
+
+  for (let i = 0; i < 12; ++i) {
+    await AnimationsController.insertFrame({
+      id: animation._id,
+      data: `https://picsum.photos/seed/${cuid()}/640/360`,
+      user: user._id
     });
-
-    users.push(user.save());
   }
-
-  return await Promise.all(users);
 }
 
-async function seedFrames(users, n = 5) {
-  let frames = []
-
-  await Promise.all(frames);
-
-  for (let i = 0; i < n; ++i) {
-    const user = sampleSize(users, 1)[0];
-
-    const width = 640, height = 360;
-    
-    const frame = new Frame({
-      user: user,
-      data: `http://picsum.photos/${width}/${height}`
-    });
-
-    frames.push(frame.save());
-  }
-
-  return await Promise.all(frames);
-}
-
-async function seedAnimations(users, n = 10) {
-  await Frame.deleteMany();
-  await Animation.deleteMany();
-
-  let animations = [];
-
-  for (let i = 0; i < n; ++i) {
-    const op = sampleSize(users, 1)[0];
-
-    const animation = new Animation({
-      creator: op,
-      frames: await seedFrames(users),
-      upvoters: sampleSize(users, 5)
-    });
-
-    animations.push(animation.save());
-  }
-
-  const result = await Promise.all(animations);
-
-  return result;
-}
 
 async function run() {
   const connection = connect();
+  const data = [];
 
-  // NOTE(kosi): First seed fake users
-  const users = await seedUsers();
-
-  console.dir(JSON.parse(JSON.stringify(users)));
-
-  // NOTE(kosi): Then seed about fake animations
-  const animations = await seedAnimations(users);
-
-  console.dir(JSON.parse(JSON.stringify(animations)), { depth: null })
+  for (let i = 0; i < 10; ++i) {
+    await seedDatum();
+  }
 
   connection.close();
 }
--- server/firebase.js
+++ server/firebase.js
@@ -2,7 +2,7 @@ const admin = require("firebase-admin");
 const serviceAccount = require("../secrets/imaginate0-firebase-adminsdk-a2t1a-9566808986.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
 
 module.exports = admin;
